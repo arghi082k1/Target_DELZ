942de4eec9969f9a55f512e252030b4f69a631e9
Arghya <arghya.deep.saha@gds.ey.com>
2024-06-24 11:27:27 +0530

    Add files and commit changes

--- target_folder_subnet/main.tf
+++ target_folder_subnet/main.tf
@@ -1,9 +1,13 @@
-# Create a virtual network
+# Resource block for creating a virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "my-vnet"
-  address_space       = ["10.0.0.0/16"]
+  name                = "my-virtual-network"
   location            = "eastus"
   resource_group_name = "my-resource-group"
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "production"
+  }
 }
 
 #
