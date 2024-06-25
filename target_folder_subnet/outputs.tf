c0c1e0d6572c0dc254de0773d5bdf104ca319562
Arghya <arghya.deep.saha@gds.ey.com>
2024-06-25 18:43:41 +0530

    Add files and commit changes

--- target_folder_subnet/outputs.tf
+++ target_folder_subnet/outputs.tf
@@ -1,6 +1,16 @@
-output "azurerm_management_group"  {
-  description = "ID of the management group"
-  value       = azurerm_management_group.mgroup.id
+output "level1_management_group_id" {
+  value       = azurerm_management_group.level1.id
+  description = "ID of the level 1 management group"
+}
+
+output "level2_management_group_id" {
+  value       = azurerm_management_group.level2.id
+  description = "ID of the level 2 management group"
+}
+
+output "level3_management_group_id" {
+  value       = azurerm_management_group.level3.id
+  description = "ID of the level 3 management group"
 }
 
 #
--- target_folder_subnet/variables.tf
+++ target_folder_subnet/variables.tf
@@ -1,22 +1,54 @@
-variable "name" {
+variable "level1_name" {
   type        = string
-  description = "Name for the management group"
+  description = "Name for level 1 management group"
 }
 
-variable "display_name" {
+variable "level1_display_name" {
   type        = string
-  description = "Friendly name for this management group. If not specified, this will be the same as the name"
+  description = "Display name for level 1 management group"
+}
+
+variable "level1_subscription_ids" {
+  type        = list(string)
+  description = "List of subscription GUIDs for level 1 management group"
+  default     = []
+}
+
+variable "level2_name" {
+  type        = string
+  description = "Name for level 2 management group"
+}
+
+variable "level2_display_name" {
+  type        = string
+  description = "Display name for level 2 management group"
+}
+
+variable "level2_subscription_ids" {
+  type        = list(string)
+  description = "List of subscription GUIDs for level 2 management group"
+  default     = []
+}
+
+variable "level3_name" {
+  type        = string
+  description = "Name for level 3 management group"
+}
+
+variable "level3_display_name" {
+  type        = string
+  description = "Display name for level 3 management group"
+}
+
+variable "level3_subscription_ids" {
+  type        = list(string)
+  description = "List of subscription GUIDs for level 3 management group"
+  default     = []
 }
 
 variable "parent_management_group_id" {
   type        = string
-  description = "The ID of the parent management group."
-}
-
-variable "subscription_ids" {
-  type        = list(string)
-  description = "A list of subscription GUIDs which should be assigned to the management group"
-  default     = []
+  description = "The ID of the parent management group"
 }
 
 #
